e096420754df3ff594765e8bc80ad807472d0c71
nazar.vrublevskyi <nazvrub@gmail.com>
2017-03-27 17:48:20 +0300

    implemented CryptoManager, updated SharedPreferencesUtil save/read access token functionality

--- app/src/main/java/co/techmagic/hr/presentation/util/SharedPreferencesUtil.java
+++ app/src/main/java/co/techmagic/hr/presentation/util/SharedPreferencesUtil.java
@@ -7,29 +7,34 @@ import android.support.annotation.NonNull;
 import com.google.gson.Gson;
 
 import co.techmagic.hr.data.entity.User;
+import co.techmagic.hr.data.manager.YealieCryptoManager;
 
 public class SharedPreferencesUtil {
 
     private static SharedPreferences prefs;
+    private static YealieCryptoManager yealieCryptoManager;
 
     private SharedPreferencesUtil() {}
 
+
     public static void init(@NonNull Context appContext) {
-        if (prefs == null) {
+        if (prefs == null && yealieCryptoManager == null) {
             prefs = appContext.getSharedPreferences(SharedPreferencesKeys.SHARED_PREFS_NAME, Context.MODE_PRIVATE);
+            yealieCryptoManager = new YealieCryptoManager();
         }
     }
 
 
     public static void saveAccessToken(String token) {
         SharedPreferences.Editor editor = prefs.edit();
-        editor.putString(SharedPreferencesKeys.ACCESS_TOKEN_KEY, token);
+        editor.putString(SharedPreferencesKeys.ACCESS_TOKEN_KEY, yealieCryptoManager.encrypt(token));
         editor.apply();
     }
 
 
     public static String getAccessToken() {
-        return prefs.getString(SharedPreferencesKeys.ACCESS_TOKEN_KEY, null);
+        String encodedAuthToken = prefs.getString(SharedPreferencesKeys.ACCESS_TOKEN_KEY, null);
+        return encodedAuthToken == null ? null : yealieCryptoManager.decrypt(encodedAuthToken, encodedAuthToken.length());
     }
 
 
