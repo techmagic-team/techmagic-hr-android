da8ac50ca0f919b017f2f7937062fdb5ac00111c
Ruslan Kuziak <ruslan.kuziak@gmail.com>
2017-05-30 19:16:06 +0300

    VersionName 1.1.9

--- app/src/main/java/co/techmagic/hr/TechMagicHrApplication.java
+++ app/src/main/java/co/techmagic/hr/TechMagicHrApplication.java
@@ -2,8 +2,11 @@ package co.techmagic.hr;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
+
 import co.techmagic.hr.data.manager.impl.NetworkManagerImpl;
 import co.techmagic.hr.presentation.util.SharedPreferencesUtil;
+import io.fabric.sdk.android.Fabric;
 
 
 public class TechMagicHrApplication extends Application {
@@ -13,6 +16,6 @@ public class TechMagicHrApplication extends Application {
         super.onCreate();
         NetworkManagerImpl.initNetworkManager(getApplicationContext());
         SharedPreferencesUtil.init(this);
-       // Fabric.with(this, new Crashlytics());
+        Fabric.with(this, new Crashlytics());
     }
 }
