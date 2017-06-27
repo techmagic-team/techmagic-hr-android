cff9fae20839531c955d7cf08e6ab27ef97ad063
Ruslan Kuziak <ruslan.kuziak@gmail.com>
2017-06-27 12:30:17 +0300

    Logic to handle fields at Details screen changed.

--- app/src/main/java/co/techmagic/hr/presentation/mvp/view/DetailsView.java
+++ app/src/main/java/co/techmagic/hr/presentation/mvp/view/DetailsView.java
@@ -10,43 +10,83 @@ public interface DetailsView extends View {
 
     void showEmail(@NonNull String email);
 
+    void hideEmail();
+
     void showSkype(@NonNull String skype);
+
+    void hideSkype();
 
     void showPhone(@NonNull String phone);
 
+    void hidePhone();
+
     void showRoom(@NonNull String room);
+
+    void hideRoom();
 
     void showDepartment(@NonNull String department);
 
+    void hideDepartment();
+
     void showLead(@NonNull String lead);
+
+    void hideLead();
 
     void showBirthday(@NonNull String birthday);
 
+    void hideBirthday();
+
     void showRelocationCity(@NonNull String city);
+
+    void hideRelocationCity();
 
     void showFirstDay(@NonNull String date);
 
+    void hideFirstDay();
+
     void showFirstDayInIt(@NonNull String date);
+
+    void hideFirstDayInIt();
 
     void showAbout(@NonNull String aboutText);
 
+    void hideAbout();
+
     void showEmergencyPhoneNumber(@NonNull String phone);
+
+    void hideEmergencyPhoneNumber();
 
     void showEmergencyContact(@NonNull String contact);
 
+    void hideEmergencyContact();
+
     void showTrialPeriodEndsDate(@NonNull String date);
+
+    void hideTrialPeriodEndsDate();
 
     void showLastWorkingDay(@NonNull String date);
 
+    void hideLastWorkingDay();
+
     void showReason(@NonNull String reason);
+
+    void hideReason();
 
     void showComment(@NonNull String comment);
 
+    void hideComment();
+
     void showVacationDays(@NonNull String dates);
+
+    void hideVacations();
 
     void showDayOff(@NonNull String dates);
 
+    void hideDayOff();
+
     void showIllnessDays(@NonNull String dates);
+
+    void hideIllnessDays();
 
     void onCopyEmailToClipboard(@NonNull String email);
 
