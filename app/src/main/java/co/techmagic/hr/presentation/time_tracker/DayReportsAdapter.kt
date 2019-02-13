f6a48546cb4747898b0618ca3a72b9a42298aa02
Alexander Kozubets <kozubets.alexander@gmail.com>
2019-02-13 10:10:31 +0200

    Implement endless scroll through days and weeks

---
+++ app/src/main/java/co/techmagic/hr/presentation/time_tracker/DayReportsAdapter.kt
@@ -0,0 +1,69 @@
+package co.techmagic.hr.presentation.time_tracker
+
+import android.graphics.Color
+import android.support.v4.content.ContextCompat
+import android.support.v7.widget.DividerItemDecoration
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import co.techmagic.hr.R
+import co.techmagic.hr.presentation.pojo.UserReportViewModel
+import co.techmagic.hr.presentation.ui.adapter.TimeReportAdapter
+import co.techmagic.hr.presentation.ui.adapter.TimeReportsClickListener
+import co.techmagic.hr.presentation.ui.view.EndlessAdapter
+
+class DayReportsAdapter : EndlessAdapter<DayReportViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DayReportViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.view_day_reports, parent, false)
+        view.layoutParams = ViewGroup.LayoutParams(parent.measuredWidth, parent.measuredHeight)
+        return DayReportViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: DayReportViewHolder, position: Int) {
+        //TODO: implement
+        when (position % 3) {
+            0 -> holder.itemView.setBackgroundColor(Color.RED)
+            1 -> holder.itemView.setBackgroundColor(Color.GREEN)
+            2 -> holder.itemView.setBackgroundColor(Color.BLUE)
+        }
+    }
+}
+
+class DayReportViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    private lateinit var rvReports: RecyclerView
+    private lateinit var reportsAdapter: TimeReportAdapter
+
+    init {
+        initRecycler()
+    }
+
+    fun showReports(reports: List<UserReportViewModel>) {
+        reportsAdapter.setNewData(reports)
+    }
+
+    private fun initRecycler() {
+        val context = itemView.context
+        rvReports = itemView.findViewById(R.id.rvReports)
+        reportsAdapter = TimeReportAdapter(context, object : TimeReportsClickListener {
+
+            override fun onTrackTimeClicked(position: Int) {
+                Toast.makeText(context, "Not implemented", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onItemClicked(position: Int) {
+                Toast.makeText(context, "Not implemented", Toast.LENGTH_SHORT).show()
+            }
+
+        })
+
+        rvReports.adapter = reportsAdapter
+
+        val dividerItemDecoration = DividerItemDecoration(context, DividerItemDecoration.VERTICAL)
+        dividerItemDecoration.setDrawable(ContextCompat.getDrawable(context!!, R.drawable.divider_time_reports)!!)
+
+        rvReports.addItemDecoration(dividerItemDecoration)
+    }
+}
--- app/src/main/java/co/techmagic/hr/presentation/time_tracker/TimeTrackerFragment.kt
+++ app/src/main/java/co/techmagic/hr/presentation/time_tracker/TimeTrackerFragment.kt
@@ -1,18 +1,14 @@
 package co.techmagic.hr.presentation.time_tracker
 
 import android.os.Bundle
-import android.support.v4.content.ContextCompat
 import android.support.v7.widget.CardView
-import android.support.v7.widget.DividerItemDecoration
+import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import co.techmagic.hr.R
-import co.techmagic.hr.presentation.pojo.UserReportViewModel
-import co.techmagic.hr.presentation.ui.adapter.TimeReportAdapter
-import co.techmagic.hr.presentation.ui.adapter.TimeReportsClickListener
+import co.techmagic.hr.presentation.ui.view.ListenablePagerSnapHelper
 import com.techmagic.viper.base.BaseViewFragment
 
 class TimeTrackerFragment : BaseViewFragment<TimeTrackerPresenter>(), TimeTrackerView {
@@ -20,10 +16,7 @@ class TimeTrackerFragment : BaseViewFragment<TimeTrackerPresenter>(), TimeTracke
         fun newInstance(): TimeTrackerFragment = TimeTrackerFragment()
     }
 
-    lateinit var rvReports: RecyclerView
-
     lateinit var btnAddTimeReport: CardView
-    private lateinit var reportsAdapter: TimeReportAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_time_tracker, container, false)
@@ -31,35 +24,18 @@ class TimeTrackerFragment : BaseViewFragment<TimeTrackerPresenter>(), TimeTracke
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        rvReports = view.findViewById(R.id.rvReports)//todo should I resolve ButterKnife code generation with kotlin issue
         btnAddTimeReport = view.findViewById(R.id.btnAddTimeReport)
 
-        initRecycler()
-    }
+        var weeks: RecyclerView = view.findViewById(R.id.rvWeeks)
+        weeks.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        val weeksPagerSnapHelper = ListenablePagerSnapHelper()
+        weeksPagerSnapHelper.attachToRecyclerView(weeks)
+        weeks.adapter = WeeksAdapter()
 
-    override fun showReports(reports: List<UserReportViewModel>) {
-        reportsAdapter.setNewData(reports)
-    }
-
-    private fun initRecycler() {
-        reportsAdapter = TimeReportAdapter(context!!, object : TimeReportsClickListener {
-
-            override fun onTrackTimeClicked(position: Int) {
-                Toast.makeText(context, "Not implemented", Toast.LENGTH_SHORT).show()
-            }
-
-            override fun onItemClicked(position: Int) {
-                Toast.makeText(context, "Not implemented", Toast.LENGTH_SHORT).show()
-            }
-
-        })
-
-        rvReports.adapter = reportsAdapter
-
-        val dividerItemDecoration = DividerItemDecoration(context, DividerItemDecoration.VERTICAL)
-        dividerItemDecoration.setDrawable(ContextCompat.getDrawable(context!!, R.drawable.divider_time_reports)!!)
-
-        rvReports.addItemDecoration(dividerItemDecoration)
+        var days: RecyclerView = view.findViewById(R.id.rvDays)
+        days.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        val daysPagerSnapHelper = ListenablePagerSnapHelper()
+        daysPagerSnapHelper.attachToRecyclerView(days)
+        days.adapter = DayReportsAdapter()
     }
 }
