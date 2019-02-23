8c2645c22d3070ea904b3251313f06d7a77c8d43
ivankerer <vaniakerer8@gmail.com>
2019-02-23 16:55:27 +0200

    Remove useless code
    https://trello.com/c/s0LEkTx5/2-time-tracker-ui-add-quotes

--- app/src/main/java/co/techmagic/hr/presentation/time_tracker/HrAppTimeTrackerPresenter.kt
+++ app/src/main/java/co/techmagic/hr/presentation/time_tracker/HrAppTimeTrackerPresenter.kt
@@ -1,6 +1,5 @@
 package co.techmagic.hr.presentation.time_tracker
 
-import android.os.Handler
 import co.techmagic.hr.domain.repository.TimeReportRepository
 import co.techmagic.hr.presentation.ui.manager.quotes.QuotesManager
 import com.techmagic.viper.Router
@@ -10,13 +9,4 @@ class HrAppTimeTrackerPresenter(
         val timeReportRepository: TimeReportRepository,
         val quotesManager: QuotesManager
 ) : BasePresenter<TimeTrackerView, Router>(), TimeTrackerPresenter {
-
-    init {
-        Handler().postDelayed({displayRandomQuote()}, 1000)
-    }
-
-    private fun displayRandomQuote(){
-        view?.showEmptyMessage(quotesManager.getRandomFormatedQuote())
-    }
-
 }
