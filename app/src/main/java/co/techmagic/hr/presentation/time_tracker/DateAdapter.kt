5d0bcc37a34e65f8100fdc31767aaeaf57908099
Alexander Kozubets <kozubets.alexander@gmail.com>
2019-03-07 01:45:25 +0200

    Add missing closing bracket

--- app/src/main/java/co/techmagic/hr/presentation/time_tracker/DateAdapter.kt
+++ app/src/main/java/co/techmagic/hr/presentation/time_tracker/DateAdapter.kt
@@ -57,6 +57,8 @@ abstract class DiscreteDateAdapter<VH : RecyclerView.ViewHolder>(
             Step.WEEK -> {
                 val diff = TimeUnit.DAYS.convert(date.firstDayOfWeekDate().timeInMillis, TimeUnit.MILLISECONDS) - TimeUnit.DAYS.convert(anchorDate.timeInMillis, TimeUnit.MILLISECONDS)
                 val offset = diff / 7
+                offset
+            }
         }.toInt()
     }
 
