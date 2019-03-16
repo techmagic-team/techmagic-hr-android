fe06ec39ad5f4a72839c5d8dd3d300dcfe1fe4b1
Alexander Kozubets <kozubets.alexander@gmail.com>
2019-03-16 11:53:24 +0200

    Reformat code in time tracker package

--- app/src/main/java/co/techmagic/hr/presentation/time_tracker/ITimeTrackerRouter.kt
+++ app/src/main/java/co/techmagic/hr/presentation/time_tracker/ITimeTrackerRouter.kt
@@ -3,6 +3,6 @@ package co.techmagic.hr.presentation.time_tracker
 import com.techmagic.viper.Router
 import java.util.*
 
-interface ITimeTrackerRouter : Router{
-    fun openDatePicker(currentDate : Calendar)
+interface ITimeTrackerRouter : Router {
+    fun openDatePicker(currentDate: Calendar)
 }
