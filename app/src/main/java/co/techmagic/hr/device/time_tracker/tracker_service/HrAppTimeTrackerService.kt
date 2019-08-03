d36dcada117abc1daa44329fd4080bbfa5d9a814
ivankerer <vaniakerer8@gmail.com>
2019-08-03 18:56:55 +0300

    Fix issue with playing notification sound after time tracker updates
    https://trello.com/c/BGx46hl8/59-on-android-o-or-later-every-notification-update-is-accompanied-by-vibration-and-or-sound-notification

--- app/src/main/java/co/techmagic/hr/device/time_tracker/tracker_service/HrAppTimeTrackerService.kt
+++ app/src/main/java/co/techmagic/hr/device/time_tracker/tracker_service/HrAppTimeTrackerService.kt
@@ -158,6 +158,7 @@ class HrAppTimeTrackerService : Service(), TimeTracker {
                     "Foreground Service Channel",
                     NotificationManager.IMPORTANCE_DEFAULT
             )
+            serviceChannel.setSound(null, null)
 
             val manager = getSystemService(NotificationManager::class.java)
             manager.createNotificationChannel(serviceChannel)
