7030bc0ab6025a6a1c99a719371a4e82259f307f
Alexander Kozubets <kozubets.alexander@gmail.com>
2019-08-25 19:24:42 +0300

    Quick fix of back stack issues

--- app/src/main/java/co/techmagic/hr/presentation/ui/fragment/FragmentCallback.java
+++ app/src/main/java/co/techmagic/hr/presentation/ui/fragment/FragmentCallback.java
@@ -1,14 +1,12 @@
 package co.techmagic.hr.presentation.ui.fragment;
 
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 
 import co.techmagic.hr.data.entity.UserProfile;
-import co.techmagic.hr.presentation.ui.ProfileTypes;
 
 public interface FragmentCallback {
 
-    void addDetailsFragment(@NonNull UserProfile userProfile, @NonNull ProfileTypes profileType, @Nullable String tag);
+    void addDetailsFragment(@NonNull UserProfile userProfile);
 
     void addCalendarFragment();
 }
